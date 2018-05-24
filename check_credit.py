ae06e2f6825a59374239cb193ef090bc801a74d1
Gustavo Castilhos Garcia <guscastles@gmail.com>
2018-05-24 22:23:49 +1000

    Updated check_credit without arg_parse

--- check_credit.py
+++ check_credit.py
@@ -2,7 +2,13 @@
 Gives the total of credits of a given period until the EOM from a starting date.
 '''
 from pandas import read_csv
-from parse_args import parse_args, dt
+from datetime import datetime as dt
+from dateutil.relativedelta import relativedelta
+#from parse_args import parse_args, dt
+
+
+def first_day_of_the_month():
+    return dt(dt.now().year, dt.now().month, 1)
 
 
 def read_data_from_csv(csv_file):
@@ -20,10 +26,10 @@ def sum_from_the_date(data, date):
     return data.loc[(data['Date'] >= date) & (data['Date'] < next_month(date)), 'Credit'].sum()
 
 
-def main(csv_file, date):
+def main(csv_file='Transactions.csv', date=first_day_of_the_month()):
     'Run the script to produce the total credit after the given date, inclusive'
-    return sum_from_the_date(read_data_from_csv(csv_file), date)
+    return f'${sum_from_the_date(read_data_from_csv(csv_file), date):,.2f}'
 
 
 if __name__ == '__main__':
-    print(main(*parse_args()))
+    print(main('Transactions.csv', dt.strptime('1/5/2018', '%d/%m/%Y')))
